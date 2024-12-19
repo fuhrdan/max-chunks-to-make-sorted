ff8f03548eaee2b6be2e0833c2fe48b2f582f490
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-12-19 07:48:21 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//*****************************************************************************
+//** 769. Max Chunks To Make Sorted                                 leetcode **
+//*****************************************************************************
+
+int maxChunksToSorted(int* arr, int arrSize) {
+    int count = 0;
+    int max = 0;
+    for(int i = 0; i < arrSize; i++)
+    {
+        max = (max > arr[i]) ? max : arr[i];
+        if(max == i) count++;
+    }
+    return count;
+}
